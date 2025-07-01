d83142bbd5ca6eac210488f52041c2729d700fa4
Ayush7web <ayushk93516@gmail.com>
2025-07-01 17:10:49 +0530

    fix the problem

--- src/components/GoogleLogin/googleLoginComp.jsx
+++ src/components/GoogleLogin/googleLoginComp.jsx
@@ -2,17 +2,19 @@ import React from 'react'
 import { GoogleLogin } from "@react-oauth/google";
 
 const GoogleLoginComp = () => {
+  const handleOnSuccess = (credentialResponse) => {
+    console.log("Login Success:", credentialResponse);
+  };
   return (
-    <div className='w-full'>
+    <div className="w-full">
       <GoogleLogin
         onSuccess={(credentialResponse) => handleOnSuccess(credentialResponse)}
         onError={() => {
           console.log("Login Failed");
         }}
       />
-      
     </div>
   );
-}
+};
 
 export default GoogleLoginComp
