45758fbaa8b698afcc14ea01a9bea7e4a912fb43
Ayush7web <ayushk93516@gmail.com>
2025-07-07 18:33:03 +0530

    all is well

--- src/App.jsx
+++ src/App.jsx
@@ -43,7 +43,7 @@ function App() {
           path="/profile/:id/activities/:postId"
           element={<SingleActivity />}
         />
-      </Routes>
+      </Routes> 
 
       <Footer />
     </div>
--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import tailwindcss from "@tailwindcss/vite";
 
 // https://vite.dev/config/
 export default defineConfig({
-  base:  "/", //  dynamic base
+  base:  "./", //  dynamic base
   plugins: [react(), tailwindcss()],
 
 });
