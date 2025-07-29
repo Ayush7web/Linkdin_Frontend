ee25786857d6d5ac115ddfc5ec06c3adbfc5d5dc
Ayush7web <ayushk93516@gmail.com>
2025-07-29 12:38:55 +0530

    hurray

--- vite.config.js
+++ vite.config.js
@@ -3,14 +3,12 @@ import react from "@vitejs/plugin-react";
 import tailwindcss from "@tailwindcss/vite";
 
 export default defineConfig({
-  test: { minThreads: 1, maxThreads: 1 },
   plugins: [react(), tailwindcss()],
   server: {
     port: 5173,
     watch: {
       usePolling: true,
       interval: 100,
-      ignored: ["**/node_modules/**", "**/.git/**"],
     },
   },
 });
